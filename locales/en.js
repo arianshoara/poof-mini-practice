97fdc8cad33daa97bdc59432a25f8c4ffedfe3c8
arian <arian311380@gmail.com>
2026-09-08 14:07:17 +0330

    feat: add English locale registry

---
+++ locales/en.js
@@ -0,0 +1,37 @@
+(function registerEnglishLocale() {
+    "use strict";
+
+    const existingRegistry =
+        window.POOF_LOCALES;
+
+    const localeRegistry =
+        existingRegistry &&
+        typeof existingRegistry === "object" &&
+        !Array.isArray(existingRegistry)
+            ? existingRegistry
+            : {};
+
+    window.POOF_LOCALES =
+        localeRegistry;
+
+    window.POOF_LOCALES.en =
+        Object.freeze({
+            "app.name":
+                "POOF Mini Practice",
+
+            "nav.home":
+                "Home",
+
+            "nav.learn":
+                "Learn",
+
+            "nav.library":
+                "Library",
+
+            "nav.cards":
+                "Cards",
+
+            "nav.account":
+                "Account"
+        });
+})();
